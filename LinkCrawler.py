38edbb1563a3d7a4ff3163d7729e7ecabdcfd227
imgomez0127 <ianm0127@hotmail.com>
2019-02-25 22:40:11 -0500

    Worked on the NetSpider to correctly call the linkcrawler to grab links. Also was able to grab the article and title however I must clean up the results and fix the encoding

--- LinkCrawler.py
+++ LinkCrawler.py
@@ -12,15 +12,12 @@ from urllib.parse import urljoin
 import time
 class LinkCrawler(object):
 
-    def __init__(self,index_url,filename,dirname,geckodriver_path,query={}):
+    def __init__(self,index_url,filename,dirname,geckodriver_path):
         self.__index_url = index_url
         self.__filename = filename
         self.__dirname = dirname
         self.__geckodriver_path = geckodriver_path
-        if(type(query) != dict):
-            raise valueerror("the input query is not of type dict")
-        self.__query = query
-
+        
     @property
     def index_url(self):
         return self.__index_url
@@ -54,47 +51,45 @@ class LinkCrawler(object):
             return self.__filename
         return self.__dirname + self.__filename
     
-    @property
-    def query(self):
-        return self.__query
-    
-    @query.setter
-    def query(self,query):
-        if(type(query) != dict):
-            raise ValueError("The input query is not of type dict")
-        self.__query = query        
-
     def __getPageHTML(self,url):
         options = Options()
         options.headless = True
         driver = webdriver.Firefox(options=options, 
                 executable_path = self.__geckodriver_path)
-        driver.get(self.__index_url) 
+        driver.set_page_load_timeout(20)
+        driver.get(url) 
         page = driver.page_source
-        driver.close()
+        driver.quit()
         return page
+
     def __constructFullURL(self,urlparams):
+        if(urlparams == {}):
+            return self.__index_url
         fullurl = self.__index_url if (self.__index_url[-1] == "/") else self.__index_url + "/"
         fullurl += "?"
         for key,value in urlparams.items():
             fullurl += key + "=" + value + "&" 
         return fullurl[:-1] 
-    def __find_links(self,a_tag_wrapper_tag,a_tag_wrapper_class,
+
+    def __find_links(self,wrapper,wrapper_attrs,
                     a_tag_attrs,urlparams):
         a_tag_list = []
         links_list = []
         fullURL = self.__constructFullURL(urlparams)
         print(fullURL)
         index_page = self.__getPageHTML(fullURL)
-        a_tag_list += BeautifulSoup(index_page,"lxml").find_all("a",attrs = a_tag_attrs)
+        index_page_soup = BeautifulSoup(index_page,"lxml")
+        wrapper_lst = index_page_soup.find_all(wrapper,attrs=wrapper_attrs)
+        for a_tag in wrapper_lst: 
+            a_tag_list += a_tag.find_all("a",attrs = a_tag_attrs)
         for a_tag in a_tag_list:
             link_url = urljoin(self.__index_url, a_tag.get('href'))
-            links_list.append(link_url)
-        print(links_list)
+            if(link_url not in links_list):
+                links_list.append(link_url)
         return links_list
 
-    def export_links(self,a_tag_attrs={},urlparams={}):
-        links_list = self.__find_links("div",{"class":"divname"},a_tag_attrs,urlparams)
+    def export_links(self,wrapper,wrapper_attrs={},a_tag_attrs={},urlparams={}):
+        links_list = self.__find_links(wrapper,wrapper_attrs, a_tag_attrs,urlparams)
         if not os.path.exists(self.__dirname):
             os.makedirs(self.__dirname)
         f = open(self.full_filename,'w')
@@ -122,5 +117,5 @@ class LinkCrawler(object):
 
 if __name__ == "__main__":
     url = "https://www.washingtonpost.com/newssearch/"
-    crawler = LinkCrawler(url,"links.txt","links/","/usr/bin/geckodriver")
-    crawler.export_links(urlparams={"query":"bitcoin"})
+    crawler = LinkCrawler(url,"links.txt","links/","/usr/bin/geckodriver") 
+    crawler.export_links("div",wrapper_attrs={"class":"pb-feed-headline"},a_tag_attrs={"class":"ng-binding"},urlparams={"query":"bitcoin"})
--- NetSpider.py
+++ NetSpider.py
@@ -15,11 +15,11 @@ class NetSpider(object):
     link_file = "links.txt"
     link_file_directory = "links/"
     queue_set = set()
-    article_count = 1
     index_url = ""
     article_file = "articles.json"
     query = {}
-    geckdriver_path = "/usr/bin/geckodriver"
+    geckodriver_path = "/usr/bin/geckodriver"
+
     def __init__(self, spider_id, data_directory,queue_link):
         self.__spider_id = spider_id
         self.__data_directory = data_directory
@@ -41,7 +41,7 @@ class NetSpider(object):
         return self.__data_directory
 
     @staticmethod
-    def startup(articles_per_page,a_tag_attrs = {}):
+    def startup(wrapper,wrapper_attrs={},a_tag_attrs = {},urlparams={}):
         if(NetSpider.index_url == ""):
             raise ValueError("There is no URL to request from")
         full_filename = NetSpider.link_file_directory + NetSpider.link_file
@@ -51,29 +51,53 @@ class NetSpider(object):
             f = open(full_filename, 'w')
             f.close()
         LinkCrawler(
-                    NetSpider.index_url,NetSpider.article_count,\
+                    NetSpider.index_url,\
                     NetSpider.link_file,NetSpider.link_file_directory,
-                    NetSpider.geckodriver_path,NetSpider.query
-                    ).export_links(articles_per_page,a_tag_attrs)
+                    NetSpider.geckodriver_path
+                    ).export_links(wrapper,wrapper_attrs,a_tag_attrs,urlparams)
         with open(full_filename,'rt') as f:
             for line in f:
                 NetSpider.queue_set.add(line.replace('\n',''))
 
-    def crawl_page(self,title_html_tags,body_html_tags,title_attrs = {}, body_attrs = {}):
-        page = requests.get(self.__queue_link)
-        page_soup = BeautifulSoup(page.text,"lxml")
-        title = page_soup.find_all(title_html_tags,attrs = title_attrs) 
-        body = page_soup.find_all(body_html_tags,body_attrs) 
+    def __getPageHTML(self,url):
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options, 
+                executable_path = NetSpider.geckodriver_path)
+        driver.set_page_load_timeout(20)
+        driver.get(url) 
+        page = driver.page_source
+        driver.quit()
+        return page
+
+    def crawl_page(self,title_html_tags,body_html_tags,title_attrs = {}, 
+                    body_attrs = {}):
+        page = self.__getPageHTML(self.__queue_link)
+        page_soup = BeautifulSoup(page,"lxml")
+        title = page_soup.find_all(title_html_tags,attrs = title_attrs)
+        body = page_soup.find_all(body_html_tags,body_attrs)
+        article = ""
+        for tag in body:
+            article += tag.get_text()        
+        print(article)
         if(title == []):
-            raise ValueError("The title has no values returned")  
+            raise ValueError("The title has no values returned") 
         if(body == []):
             raise ValueError("The body has no values returned")
-        dataJson = json.dumps({"title": title, "body":body})
+        dataJson = json.dumps({"title": title[0].get_text(), "article":article})
         return dataJson 
 
     def getArticleAsJSON(self,title_html_tags,body_html_tags,title_attrs = {},
-                    body_attrs = {}):
+                        body_attrs = {}):
         jsonData = self.crawl_page(title_html_tags,body_html_tags,
                                     title_attrs,body_attrs) 
         return jsonData                
     
+if __name__ == "__main__":
+    url = "https://www.washingtonpost.com/newssearch/"
+    query = {"query":"bitcoin"}
+    NetSpider.index_url = url
+    NetSpider.startup("div",wrapper_attrs={"class":"pb-feed-headline"},a_tag_attrs={"class":"ng-binding"},urlparams={"query":"bitcoin"})
+    print(NetSpider.queue_set)
+    spider = NetSpider(1,"data/",NetSpider.queue_set.pop())
+    print(json.loads(spider.crawl_page("h1","p",title_attrs = {"itemprop":"headline"})))
